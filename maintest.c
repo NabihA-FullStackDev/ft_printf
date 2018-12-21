50c3ae352e87113267af0150f3893c8201b93d13
Justin CAPIK <jucapik@e3r5p3.42.fr>
2018-12-21 12:11:59 +0100

    vla

--- maintest.c
+++ maintest.c
@@ -6,7 +6,7 @@
 /*   By: jucapik <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/14 13:15:52 by jucapik           #+#    #+#             */
-/*   Updated: 2018/12/21 10:29:22 by jucapik          ###   ########.fr       */
+/*   Updated: 2018/12/21 12:10:20 by jucapik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ int		main()
 
 	ft_printf("abcde%sfghi%cjklmn%s\n", "1234", '5', "91011");
 	shrt = shrt -1;
-	//printf("res = %-10d\n", 1234);
-	//printf("str = %1s\n", "abcdefghijk");
-	return (0);
+	/*printf("oct? = %ld\n", 2147483648);
+	printf("oct? = % -5i\n", 012);
+	printf("oct? = % -5i\n", 0x12);
+	*/return (0);
 }
