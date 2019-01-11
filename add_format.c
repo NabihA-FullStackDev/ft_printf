c07d335e8b1028a2decdad994fb78328926fb320
Justin CAPIK <jucapik@e3r5p3.42.fr>
2019-01-11 16:38:14 +0100

    fonction pour format diouxX ecris mais pas teste

--- add_format.c
+++ add_format.c
@@ -6,30 +6,38 @@
 /*   By: jucapik <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/10 09:34:19 by jucapik           #+#    #+#             */
-/*   Updated: 2019/01/11 09:44:36 by jucapik          ###   ########.fr       */
+/*   Updated: 2019/01/11 16:37:22 by jucapik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft/libft.h"
 
-static void	case1(char **nbr, t_param *p, int size, int c)
+static void	case1(char *nbr, t_param *p, int size, int c)
 {
-	char *zeros;
-	char *tmp;
+	char	*tofill;
+	char	*tmp;
+	int		big;
 
-	zeros = (char *)malloc(sizeof(char) * (p->apres - size + 1));
-	i = -1;
-	while (++i < p->apres - size)
-		zeros[i] = c;
-	zeros[i] = '\0';
-	tmp = ft_strjoin(zeros, nbr);
-	ft_memdel(&zeros);
-	ft_memdel(nbr);
+	big = (p->apres > p->avant) ? p->apres : p->avant;
+	if (p->flags & neg || p->flags & plus || p->flags & espace)
+		++big;
+	tofill = (char *)malloc(sizeof(char) * (big - size + 1));
+	i = 0;
+	if (c == '0')
+		signe(tofill, &i, p);
+	while (i < big - size - 1)
+		tofill[i++] = c;
+	if (c == ' ' && signe(tofill, &i, p) == FALSE)
+		tofill[i++] = ' ';
+	tofill[i] = '\0';
+	tmp = ft_strjoin(tofill, nbr);
+	ft_memdel(&tofill);
+	ft_memdel(&nbr);
 	return (tmp);
 }
 
-static void	case2(char **nbr, t_param *p, int size)
+static void	case2(char *nbr, t_param *p, int size)
 {
 	char	*tofill;
 	char	*tmp;
@@ -37,39 +45,39 @@ static void	case2(char **nbr, t_param *p, int size)
 
 	tofill = (char *)malloc(sizeof(char) * (p->avant - size + 1));
 	i = -1;
-	while (++i < p->avant - p->apres)
+	while (++i < p->avant - p->apres - 1)
 		tofill[i] = ' ';
+	if (signe(tofill, &i, p) == FALSE)
+		tofill[i++] = ' ';
 	while (++i < p->avant - size)
 		tofill[i] = '0';
 	tofill[i] = '\0';
 	tmp = ft_strjoin(tofill, nbr);
 	ft_memdel(&tofill);
-	ft_memdel(nbr);
+	ft_memdel(&nbr);
 	return (tmp);
 }
 
-static void	signe(char *nbr, t_param *p)
+bln			signe(char *tofill, int *i, t_param *p)
 {
 	char *tmp;
 
-	if (p->flags & plus)
-	{
-		tmp = ft_strjoin("+", nbr);
-		ft_memdel(nbr);
-		nbr = tmp;
-	}
+	if (p->flags & neg)
+		tofill[(*i)++] = '-';
 	else if (p->flags & plus)
-	{
-		tmp = ft_strjoin(" ", nbr);
-		ft_memdel(nbr);
-		nbr = tmp;
-	}
+		tofill[(*i)++] = '+';
+	else if (p->flags & espace)
+		tofill[(*i)++] = ' ';
+	else
+		return (FALSE);
+	return (TRUE);
 }
 
-void		add_format(char **nbr, t_param *p)
-{
+void		add_format(char *nbr, t_param *p)
+{// TODO ne pas envoyer un nombre avec '-' mais activer le flag p->neg
 	int		size;
 	int		i;
+	bln		negpos;
 
 	size = ft_strlen(nbr);
 	if (!(p->flags & moins))
@@ -83,6 +91,5 @@ void		add_format(char **nbr, t_param *p)
 	}
 	else
 		add_format_helper(nbr, p, size);
-	if ((p->flags & plus || p->flags & espace) && nbr[0] != '-')
-		signe(nbr, p);
+	return (nbr);
 }
--- add_format_helper.c
+++ add_format_helper.c
@@ -6,10 +6,59 @@
 /*   By: jucapik <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/11 09:06:56 by jucapik           #+#    #+#             */
-/*   Updated: 2019/01/11 09:44:33 by jucapik          ###   ########.fr       */
+/*   Updated: 2019/01/11 16:37:23 by jucapik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*add_format_helper(char *nbr, t_param *p, int size)
+#include "FdF.h"
+
+static char	*case1(char *nbr, int big)
 {
-	if
+	char	*tmp;
+	char	*tofill;
+	int		i;
+
+	tofill = ft_strnew(big);
+	i = 0;
+	signe(tofill, &i, p);
+	while (i < big)
+		tofill[i++] = '0';
+	tmp = ft_strjoin(tofill, nbr);
+	ft_memdel(tofill);
+	ft_memdel(nbr);
+	return (tmp);
+}
+
+static char *case2(char *nbr, int big, t_param *p)
+{
+	char	*tmp;
+	char	*tofill;
+	int		size;
+	int		i;
+
+	size = p->avant - big;
+	tofill = ft_strnew(size);
+	i = -1;
+	while (++i < size)
+		tofill[i] = ' ';
+	tmp = ft_strjoin(nbr, tofill);
+	ft_memdel(tofill);
+	ft_memdel(nbr);
+	return (tmp);
+}
+
+
+char		*add_format_helper(char *nbr, t_param *p, int size)
+{
+	char	*tmp;
+	int		big;
+
+	big = p->apres - size;
+	big += (p->flags & neg || p->flags & plus || p->flags & espace) ? 1 : 0;
+	if (big > 0)
+		nbr = case1(nbr, big);
+	big = ft_strlen(nbr);
+	if (p->avant > big)
+		nbr = case2(nbr, big, p);
+	return (nbr);
+}
