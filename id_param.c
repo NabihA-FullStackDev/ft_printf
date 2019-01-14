536ebcf2e2deabc97b9c1e544cc9e7cd78f715ac
Nabih ALI <naali@student.42.fr>
2019-01-14 13:47:09 +0100

    retrait du moins dans itoa base

--- id_param.c
+++ id_param.c
@@ -6,7 +6,7 @@
 /*   By: naali <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/03 15:43:52 by naali             #+#    #+#             */
-/*   Updated: 2019/01/09 20:21:11 by naali            ###   ########.fr       */
+/*   Updated: 2019/01/14 13:46:22 by naali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,22 @@
 
 char		*d_param(t_param *p)
 {
+	int		signe;
 	char	*nbr;
 
+	if ((int)p->arg < 0)
+		signe = 1;
 	nbr = convert_param_for_itoa(p, 10);
 	return (nbr);
 }
 
 char		*i_param(t_param *p)
 {
+	int		signe;
 	char	*nbr;
 
+	if ((int)p->arg < 0)
+		signe = 1;
 	nbr = convert_param_for_itoa(p, 10);
 	return (nbr);
 }
